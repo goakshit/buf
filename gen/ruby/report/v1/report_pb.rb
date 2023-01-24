305507fec838afa3add4da212a1e11f69dad7080
Akshit Dhar <akshit.dhar@mondu.ai>
2023-01-24 11:22:03 +0100

    feat(workflow): please work this time 2

--- gen/ruby/report/v1/report_pb.rb
+++ gen/ruby/report/v1/report_pb.rb
@@ -48,6 +48,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       optional :stupider, :string, 19, json_name: "stupider"
       optional :stupiderr, :string, 20, json_name: "stupiderr"
       optional :stupiderrr, :string, 21, json_name: "stupiderrr"
+      optional :stupiderrrr, :string, 22, json_name: "stupiderrrr"
     end
     add_message "report.v1.DeleteReportResponse" do
     end
