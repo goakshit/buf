87fe9016b9f03478510249bf55f2779fc36eab3f
Akshit Dhar <66997014+goakshit@users.noreply.github.com>
2023-01-25 22:37:04 +0100

    feat: test (#71)

--- gen/ruby/report/v1/report_pb.rb
+++ gen/ruby/report/v1/report_pb.rb
@@ -68,6 +68,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       optional :test5, :string, 39, json_name: "test5"
       optional :test6, :string, 40, json_name: "test6"
       optional :test7, :string, 41, json_name: "test7"
+      optional :test8, :string, 42, json_name: "test8"
     end
     add_message "report.v1.DeleteReportResponse" do
     end
