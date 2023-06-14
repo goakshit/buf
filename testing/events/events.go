5f524c3190d5c2c627f85314e30cbf393e3601f8
Akshit Dhar <akshit.dhar@mondu.ai>
2023-06-14 18:57:51 +0200

    feat(testing): new test flow

---
+++ testing/events/events.go
@@ -0,0 +1,12 @@
+package events
+
+import (
+	paymentv1 "github.com/goakshit/buf/gen/go/payment/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+var (
+	Events = []proto.Message{
+		&paymentv1.Payment{},
+	}
+)
